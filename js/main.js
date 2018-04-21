9f5670ccaa2bd573c495902c43fb1fb9f6c32b1a
Sorokin Dmitrii <tipalol2@gmail.com>
2018-04-21 14:18:46 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,64 +1,42 @@
+Vue.component('modal', {
+  template: '#modal-template',
+  methods: {
+
+  }		
+})
 var app = new Vue({
 	el: '#app',
 	data: {
-		none_visible: false,
-		reg_status: true,
-		nick: ''
+		showModal: false,
+		modalText: 'Название заметки',
+		notes: [
+			
+		]
 	},
-	components: {
-		chat: {
-			template: '#template_chat',
-			data: function() {
-				return {
-					messages_count: 0,
-					messages: [{}],
-					current_message_text: '',
-					nick: app.nick,
-				}
-			},
-			methods: {
-				printMessage: function(_nick, _message) {
-					var new_message = {
-						nick: _nick,
-						message: _message
-					}
-					this.messages.push(new_message);
-					this.messages_count++
-				},
-				sendMessage: function() {
-					this.printMessage(app.nick, this.current_message_text)
-					this.current_message_text = ''
-				},
-				chars: function() {
-						//alert(this.current_message_text.lenght)
-						return this.current_message_text.lenght
-				}
-			},
-			mounted: function() {
-				this.printMessage('Администрация', 'Приветсвуем вас в нашем анонимном чате!')
-				this.printMessage('Боб', 'Привет всем!')
-				this.printMessage('Джон', 'Хай, как дела?')
-				this.printMessage('Алли', 'Всем добрый вечер')
-			}
-		},
-		reg_form: {
-			template: '#reg-form-template',
-			data: function() {
-				return {
-					nick: ''
-				}
-			},
-			methods: {
-				submitReg: function() {
-					this.$emit('registration_confirmed', this.nick)
-				}
-			}
+	mounted: function() {
+		console.log('App running')
+		if (localStorage['notes'] == undefined) {
+			console.log('First running')
+			localStorage['notes'] = []
+		} else {
+			this.notes = JSON.parse(localStorage['notes'])
+			console.log('Notes loaded')
 		}
 	},
 	methods: {
-		enableChat: function(nick) {
-			this.reg_status = false
-			this.nick = nick
+		createNote: function() {
+			console.log('Creating new note')
+			showModal = true
+		},
+		close: function() {
+			this.notes.push({
+				name: this.modalText.split(' ')[0],
+				text: this.modalText
+			})
+			localStorage['notes'] = JSON.stringify(this.notes)
+			this.showModal = false
+			console.log('closed')
+			this.modalText = ''
 		}
 	}
 })
